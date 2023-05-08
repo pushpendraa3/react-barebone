3976d1b59b1e9144c6095e662e55851cc910a058
Pushpendra Singh Bhadoriya <pushpendrasingh.91650@gmail.com>
2023-05-08 12:38:01 +0530

    progress:1

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './App.css';
+
+
+const heading = <h1>Namaste React</h1>
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
